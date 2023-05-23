7dc6b7e8aca6734131018c9c388d214d3fae46c1
tareq_abuaqlain <tareqabuaqlain1@gmail.com>
2023-05-23 15:34:07 +0300

    Change the style from customer blade to product image blade

--- resources/views/components/product-image.blade.php
+++ resources/views/components/product-image.blade.php
@@ -1,11 +1,12 @@
 @if (isset($product->images[$index]))
-<a href={{route('products.show', $product)}}>
-    @if(str_contains($product->images[$index], 'dummy'))
-        <img {{ $attributes->merge(["src" => $product->images[$index], "alt" => "IMG-PRODUCT"]) }} >
+<a href="{{ route('products.show', $product) }}">
+    @if (str_contains($product->images[$index], 'dummy'))
+    <img {{ $attributes->merge(["src" => $product->images[$index], "alt" => "IMG-PRODUCT", "style" => "object-fit: contain; width: 142%;"]) }}>
     @else
-        <img {{ $attributes->merge(["src" => asset('storage/' . $product->images[$index]), "alt" => "IMG-PRODUCT"]) }} >
+    <img {{ $attributes->merge(["src" => asset('storage/' . $product->images[$index]), "alt" => "IMG-PRODUCT", "style" => "object-fit: contain; width: 142%;"]) }}>
     @endif
+</a>
 @else
-    <img {{ $attributes->merge(["src" => asset('images/product-detail-01.jpg'), "alt" => "IMG-PRODUCT"]) }} >
+<img {{ $attributes->merge(["src" => asset('images/product-detail-01.jpg'), "alt" => "IMG-PRODUCT", "style" => "object-fit: contain; width: 142%;"]) }}>
 </a>
 @endif
